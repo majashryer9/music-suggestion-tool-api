bcdda71596f0fd9b425fba1ce0826ae3443377d7
Maja Shryer <majashryer@gmail.com>
2020-11-14 14:43:29 -0600

    Use correct env variable.

--- src/util/neo4j-session.ts
+++ src/util/neo4j-session.ts
@@ -1,8 +1,8 @@
 import neo4j from "neo4j-driver";
 
 const driver = neo4j.driver(
-  process.env.DB_USER || "http://localhost:7474",
-  neo4j.auth.basic(process.env.DB_USER || "user", process.env.DB_PASS || "pass")
+  process.env.DB_URI || "http://localhost:7474",
+  neo4j.auth.basic(process.env.DB_USER || "neo4j", process.env.DB_PASS || "pass")
 );
 
 export const session = driver.session();
