30a9bae1f69d61d34dd77f853b2c72a2b05ac1fc
Maja Shryer <majashryer@gmail.com>
2021-02-13 13:28:03 -0600

    Move dotenv.config() up.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
 import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import { songRouter } from "./routers/song-router";
-
-dotenv.config();
 
 const app = express();
 
